5d8701bf37edec9189411a8ed90b1638005e7174
herccc <101526374+herccc@users.noreply.github.com>
2022-03-13 16:22:55 -0400

    Update scrape_avatar.py

--- scrape_avatar.py
+++ scrape_avatar.py
@@ -5,9 +5,3 @@ id = str('your id here')
 payload = {'userId':id, 'width':420, 'height':420, 'format':"png" }
 r = requests.get('https://www.roblox.com/bust-thumbnail/image?', params = payload)
 avatar = r.content
-
-
-
-
-
-
