9a70f9b1725d8139aad0c4eee194c953cb098287
herccc <101526374+herccc@users.noreply.github.com>
2022-03-13 16:14:53 -0400

    Add files via upload

---
+++ scrape_avatar.py
@@ -0,0 +1,13 @@
+import requests
+
+id = str('your id here')
+
+payload = {'userId':id, 'width':420, 'height':420, 'format':"png" }
+r = requests.get('https://www.roblox.com/bust-thumbnail/image?', params = payload)
+avatar = r.content
+
+
+
+
+
+
